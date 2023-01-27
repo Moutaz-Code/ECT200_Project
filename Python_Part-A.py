54d25d02e32cc181d321762bb9320ee3b191608d
Moutaz-Code <motaz.p.r.n@gmail.com>
2023-01-27 21:18:15 +0400

    lkj

--- Python_Part-A.py
+++ Python_Part-A.py
@@ -1,1 +1,7 @@
-
+from tkinter import *
+from tkinter.ttk import * # importing tkinter module to allow us to make a simple GUI
+import math as meth # importing a math module to allow us to do.. math
+def Ticket_Book():
+    # 1 way
+    # Return
+    # Multi-City
