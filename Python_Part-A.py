8eefd31417fbc73ee86d96c5aaeb8f31c63de45c
Moutaz-Code <motaz.p.r.n@gmail.com>
2023-01-27 22:13:44 +0400

    ckjckj

--- Python_Part-A.py
+++ Python_Part-A.py
@@ -1,4 +1,5 @@
-import math as m
-import random as rand
 import tkinter as tk
-import tkinter.ttk as ttk
+import tkinter.ttk as ttk # importing tkinter module to allow us to make a simple GUI
+import math as meth # importing a math module to allow us to do.. math
+def hello()
+"""
