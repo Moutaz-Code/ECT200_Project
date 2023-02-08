a82023e760579608080b43b31c4580eff4ba439f
Moutaz-Code <motaz.p.r.n@gmail.com>
2023-02-08 23:46:10 +0400

    update

--- Python_Part-A.py
+++ Python_Part-A.py
@@ -11,19 +11,27 @@ from datetime import datetime
 
 import os
 os.system('cls')
-
-tripDirection = [1, 2] #1 way or 2 ways
-tripDistance = eval(input("trip distance"))
-tripClass = {
-    "Economy Class":300,
-    "Business Class": 850,
-    "First Class": 1600
+##
+base_fare
+trip_direction = [1, 2]  # 1 or 2 ways
+distance
+rate_per_km
+class_premium = [300, 850, 1600]  # Economy, Business, and first classes
+demand_factor  # In this iteration, the multiplier should be between 0.7 and 1.4. This variable is a multiplier for days of the week, to simulate demand on a specific day which affects price.
+age_factor = {
+    "Adult": 200,
+    "Infant": 75
 }
-demandFactor = [] # This variable is a multiplier for days of the week, to simulate demand on a specific day which affects price.
-# In this iteration, the multiplier should be between 0.7 and 1.4. 
+
+
+##
+tripDistance = eval(input("trip distance"))  # this is a placeholder
+demandFactor = []
+
 
 def bookFlightCost():
-    ticketCost = ((tripDirection * tripDistance)+tripClass)*demandFactor
+    ticketCost = (base_fare + (trip_direction*(rate_per_km*distance)
+                               ) + class_premium+age_factor)*demand_factor
 
 
 def Reschedule():
@@ -63,33 +71,58 @@ frame3 = ttk.Frame(notebook, width=screen_width, height=screen_height)
 
 # frames in tab 1
 frameText1 = ttk.Frame(frame1)
-# change width and relief later on, use for now to make organising easier
-frameText1['borderwidth'] = 5
-frameText1['relief'] = 'sunken'
+# change width and relief later on, use for now to make organising easier=
 frameText1.pack()
 
 # frames in tab 2
 frameText2 = ttk.Frame(frame2)
 # change width and relief later on, use for now to make organising easier
-frameText2['borderwidth'] = 5
-frameText2['relief'] = 'sunken'
 frameText2.pack()
 
 # frames in tab 3
 frameText3 = ttk.Frame(frame3)
 # change width and relief later on, use for now to make organising easier
-frameText3['borderwidth'] = 5
-frameText3['relief'] = 'sunken'
 frameText3.pack()
 
 # labels for reference
-label1 = ttk.Label(frameText1, text="bruh")
-label2 = ttk.Label(frameText2, text="bruh The sequel")
-label3 = ttk.Label(frameText3, text="bruh The Prequel")
+label1 = ttk.Label(
+    frameText1, text="Welcome to the booking tab! Here you can book a flight ticket using many options.")
+label2 = ttk.Label(
+    frameText2, text="Welcome to the managing tab! Here you can reschedule or cancel your ticket.")
+label3 = ttk.Label(
+    frameText3, text="Welcome to the status tab! Here you can check the status of your flight and ticket.")
 
-label1.pack(fill='both', expand=True)
-label2.pack(fill='both', expand=True)
-label3.pack(fill='both', expand=True)
+label1.grid(column=1, row=1)
+label2.grid(column=1, row=1)
+label3.grid(column=1, row=1)
+
+radio1
+radio2
+radio3
+
+radio1.grid()
+radio2.grid()
+radio3.grid()
+
+combobox1
+combobox2
+
+combobox1.grid()
+combobox2.grid()
+
+dropdownlist
+
+dropdownlist.grid()
+
+dropdownlist2
+# this is for the passengers
+dropdownlist2.grid()
+
+datepicker_departure
+datepicker_departure.grid()
+
+datepicker_return
+datepicker_return.grid()
 
 frame1.pack(fill='both', expand=True)
 frame2.pack(fill='both', expand=True)
